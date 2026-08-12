f399ad2d0eaf058c65cb7fa8eee5d37aeab613c4
Lokesh Kumar <lokeshchennaparapu@gmail.com>
2026-08-12 11:49:43 +0530

    this is my first coding round

---
+++ myfirstjavaproject/src/com/corejava/FianalPrizes.java
@@ -0,0 +1,36 @@
+package com.corejava;
+import java.util.*;
+public class FianalPrizes {
+	
+	
+
+
+
+	   public static int[] finalPrices(int[] prices) {
+	        int n = prices.length;
+	        int[] result = prices.clone();
+
+	        for (int i = 0; i < n; i++) {
+	            for (int j = i + 1; j < n; j++) {
+
+	                if (prices[j] <= prices[i]) {
+	                    result[i] = prices[i] - prices[j];
+	                    break;
+	                }
+	            }
+	        }
+
+	        return result;
+	    }
+
+	    public static void main(String[] args) {
+
+	        int[] prices = {8, 4, 6, 2, 3};
+
+	        int[] result = finalPrices(prices);
+
+	        System.out.println(Arrays.toString(result));
+	    }
+	}
+
+
