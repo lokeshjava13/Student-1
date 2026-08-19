db731b8412e11390cdde3a3acfbbdbf4b8808a1e
Lokesh Kumar <lokeshchennaparapu@gmail.com>
2026-08-19 21:33:20 +0530

    aug 19 th assignment

---
+++ myfirstjavaproject/src/com/languvagefundmentals/ObjectDemo.java
@@ -0,0 +1,41 @@
+package com.languvagefundmentals;
+
+public class ObjectDemo {
+	
+  
+
+	    static ObjectDemo d = new ObjectDemo();
+
+	    ObjectDemo() {
+	        method1();
+	        method2();
+	        method3();
+	        method4();
+	    }
+
+	    void method1() {
+	        System.out.println("This is Method 1");
+	    }
+
+	    void method2() {
+	        System.out.println("This is Method 2");
+	    }
+
+	    void method3() {
+	        System.out.println("This is Method 3");
+	    }
+
+	    void method4() {
+	        System.out.println("This is Method 4");
+	    }
+
+	    public static void main(String[] args) {
+	    }
+	
+
+
+		
+
+	}
+
+
